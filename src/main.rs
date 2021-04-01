302194d760c17c25e6db5530e67aba44f442fc2f
Jason Rodney Hansen <jasonrodneyhansen@gmail.com>
2021-04-01 10:15:41 -0600

    Add ability to set optimize method

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,38 @@ use std::convert::Infallible;
 use tokio::sync::oneshot;
 use warp::Filter;
 
+#[derive(Deserialize, Debug, Clone, Copy)]
+#[serde(rename_all = "camelCase")]
+enum OptimizeMethod {
+    Guillotine,
+    Nested,
+}
+
 #[derive(Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 struct OptimizerInput {
-    random_seed: u64,
+    method: OptimizeMethod,
+    random_seed: Option<u64>,
     cut_width: usize,
     stock_pieces: Vec<StockPiece>,
     cut_pieces: Vec<CutPiece>,
+}
+
+impl Into<Optimizer> for OptimizerInput {
+    fn into(self) -> Optimizer {
+        let mut optimizer = Optimizer::new();
+        optimizer
+            .set_random_seed(self.random_seed.unwrap_or(1))
+            .set_cut_width(self.cut_width)
+            .add_stock_pieces(self.stock_pieces)
+            .add_cut_pieces(self.cut_pieces);
+        optimizer
+    }
+}
+
+#[derive(Serialize, Debug)]
+struct ErrorMessage {
+    error: String,
 }
 
 #[tokio::main]
@@ -23,25 +48,18 @@ async fn main() {
     warp::serve(optimize).run(([127, 0, 0, 1], 3030)).await;
 }
 
-#[derive(Serialize, Debug)]
-struct ErrorMessage {
-    error: String,
-}
-
 async fn optimize(input: OptimizerInput) -> Result<impl warp::Reply, Infallible> {
     let (tx, rx) = oneshot::channel();
 
-    tokio::task::spawn_blocking(move || {
-        let result = Optimizer::new()
-            .set_random_seed(input.random_seed)
-            .set_cut_width(input.cut_width)
-            .add_stock_pieces(input.stock_pieces)
-            .add_cut_pieces(input.cut_pieces)
-            .optimize_guillotine(|_| {});
+    rayon::spawn(move || {
+        let method = input.method;
+        let optimizer: Optimizer = input.into();
+        let result = match method {
+            OptimizeMethod::Guillotine => optimizer.optimize_guillotine(|_| {}),
+            OptimizeMethod::Nested => optimizer.optimize_nested(|_| {}),
+        };
         let _ = tx.send(result);
-    })
-    .await
-    .unwrap();
+    });
 
     match rx.await {
         Ok(result) => match result {
@@ -49,8 +67,8 @@ async fn optimize(input: OptimizerInput) -> Result<impl warp::Reply, Infallible>
             Err(cut_optimizer_2d::Error::NoFitForCutPiece(cut_piece)) => {
                 Ok(warp::reply::json(&ErrorMessage {
                     error: format!(
-                        "Cut piece with external_id = {} doesn't fit any stock pieces",
-                        cut_piece.external_id
+                        "The following cut piece doesn't fit any stock pieces: {:?}",
+                        cut_piece
                     ),
                 }))
             }
